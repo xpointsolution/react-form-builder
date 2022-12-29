a510d4c6a7f7b5b3821778535c3b22dccc7c5743
Alexander Mudzo <alexander.mudzo@gmail.com>
2022-12-29 14:43:55 +0100

    added missing localization

--- src/form-elements/component-label.jsx
+++ src/form-elements/component-label.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import myxss from './myxss';
+import IntlMessages from '../language-provider/IntlMessages';
 
 const ComponentLabel = (props) => {
   const hasRequiredLabel = (props.data.hasOwnProperty('required') && props.data.required === true && !props.read_only);
@@ -7,7 +8,7 @@ const ComponentLabel = (props) => {
   return (
     <label className={props.className || ''}>
       <span dangerouslySetInnerHTML={{ __html: labelText }}/>
-      {hasRequiredLabel && <span className="label-required badge badge-danger">Required</span>}
+      {hasRequiredLabel && <span className="label-required badge badge-danger"><IntlMessages id='required' /></span>}
     </label>
   );
 };
